086f185a74047bf1f92f5d2a7cf22944a25e0be6
Seu nome aqui <seuemail@email.com>
2025-05-13 17:37:27 -0300

    alteração imagens

--- src/components/Colleger/index.jsx
+++ src/components/Colleger/index.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import './Colleger.css'
+import icon from '../../../public/img/college-icon-1-100x100.png';
+import icon1 from '../../../public/img/empregos-100x100.png';
+import icon2 from '../../../public/img/salario-100x100.png';
+import image from '../../../public/img/sou-colleger.png';
 export default function Colleger() {
 
    return (
@@ -7,23 +11,23 @@ export default function Colleger() {
     <div className="containerCollege">
     <div>    
     <div className="cardsCollege">
-        <img src="/img/college-icon-1-100x100.png" alt="icone grafico" width={50} height={50}/>
+        <img src={icon} alt="icone grafico" width={50} height={50}/>
         <p>Mercado em ascensão</p>
     </div>
 
     <div className="cardsCollege">
-        <img src="/img/empregos-100x100.png" alt="icone grafico" width={50} height={50}/>
+        <img src={icon1} alt="icone grafico" width={50} height={50}/>
         <p>Mais de 400 mil vagas disponíveis</p>
     </div>
 
     <div className="cardsCollege">
-        <img src="/img/salario-100x100.png" alt="icone grafico" width={50} height={50}/>
+        <img src={icon2} alt="icone grafico" width={50} height={50}/>
         <p>Salários robustos de até R$ 35 mil</p>
     </div>
     </div>
 
     <div>
-        <img className="imageCollege" src="/img/sou-colleger.png" alt="Sou Colleger"/>
+        <img className="imageCollege" src={image} alt="Sou Colleger"/>
     </div>
     </div>
     </>    
